e250124714d3ade779f75d3660b7bcd69fa65e49
Ben Garcia <ben@seebenprogram.com>
2018-03-03 12:50:16 -0500

    Begin yelpApi object

--- assets/javascript/api-yelp.js
+++ assets/javascript/api-yelp.js
@@ -1,3 +1,9 @@
-/*api key 
-6tWDauoNUL_rL2vdvqzgnXWAgDLvTcjTw_cY0nNUiOJ_uYsG7C1t-v_2GTGn-UAuTF6WvzEybg9ln76ki2X90pNC-4hCq6Fr2CsZnANJXx1oWAp1wIejwnMCfdKaWnYx
+const yelpAPI = {
+  apiKey: "6tWDauoNUL_rL2vdvqzgnXWAgDLvTcjTw_cY0nNUiOJ_uYsG7C1t-v_2GTGn-UAuTF6WvzEybg9ln76ki2X90pNC-4hCq6Fr2CsZnANJXx1oWAp1wIejwnMCfdKaWnYx"
+};
+
+/*
+endpoint: https://api.yelp.com/v3/businesses/search
+docs: https://www.yelp.com/developers/documentation/v3/business_search
+https://www.yelp.com/developers/documentation/v3/authentication
 */
