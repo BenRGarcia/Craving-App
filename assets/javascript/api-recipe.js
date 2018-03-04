a3fab40e614ed821c0940fdb4e9e1938b5aea64b
Your Name <melissarburnham@gmail.com>
2018-03-04 17:34:10 -0500

    adding layout of js

--- assets/javascript/api-recipe.js
+++ assets/javascript/api-recipe.js
@@ -1,14 +1,20 @@
-var queryURL = "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=4c3b3108cb8ae8b884d83deac5b1a363&q=shredded%20chicken";
+$(document).ready(function(){
+
+var queryURL = "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=4c3b3108cb8ae8b884d83deac5b1a363&q=";
+var craving = $(this).attr("data-name");
+
+function displayRecipes(){
 
     $.ajax({
         url: queryURL,
         method: "GET"
-      })
-        .then(function(response) {
+      }).then(function(response) {
 
         // Log the queryURL
         console.log(queryURL);
 
         // Log the resulting object
         console.log(response);
-      });
+      })
+    } 
+ });
