05ee79886cef7fbbd7ac407bac5342fec3402ca8
Your Name <melissarburnham@gmail.com>
2018-03-05 19:37:55 -0500

    fixed recipe api

--- assets/javascript/api-recipe.js
+++ assets/javascript/api-recipe.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
 
-var queryURL = "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=4c3b3108cb8ae8b884d83deac5b1a363&q=";
+var queryURL = "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=4c3b3108cb8ae8b884d83deac5b1a363&q=apples";
 var craving = $(this).attr("data-name");
 
-function displayRecipes(){
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+          // Log the queryURL
+    console.log(queryURL)
+        });
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-      })
-    } 
- });
+  })
+        
